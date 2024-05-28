399a88f8f6bd22a3906aa37e1ea693f8aaf84f5f
ramilka <lilya1976.03.03@gmail.com>
2024-05-28 17:41:52 +0600

    Сделал кнопку сброса, а также подсчет попыток. Также сделал доп задание, где при нахождении кольца надо остановить игру и человек должен нажать кнопку сброса.

--- src/components/Desk/Desk.tsx
+++ src/components/Desk/Desk.tsx
@@ -33,22 +33,33 @@ const Desk = () => {
         });
     };
 
+    const clickedObjects = items.map((item) => {return item.clicked});
+    const resultTries = clickedObjects.filter((item) => item === true)
+
+    const hasItemObjects = items.map((item) => {return item.hasItem});
+    const findRing = () => {
+        for (let i = 0; i < hasItemObjects.length; i++) {
+            if (clickedObjects[i] && hasItemObjects[i]) {
+                return 1;
+            }
+        }
+    }
+
     return (
         <>
-            <div style={{
-                display: "flex",
-                padding: "10px",
-                gap: "3px",
-                border: "1px solid black",
-                width: "318px",
-                flexWrap: "wrap"
-            }}>
-                {items.map((item) => (
-                    <OneBlock key={item.id} hasItem={item.hasItem} clicked={item.clicked}
-                              divClick={() => clickedItems(item.id)}/>
-                ))}</div>
+            {findRing() === 1 ?
+                <div style={{ display: "flex", padding: "10px", gap: "3px", border: "1px solid black", width: "318px", flexWrap: "wrap" }}>
+                    Кольцо найдено с {resultTries.length} попытки! Можно продолжать нажимать только после "сброса".
+                    {items.map((item) => (
+                        <OneBlock key={item.id} hasItem={item.hasItem} clicked={item.clicked} />))}
+                </div>
+                :
+                <div style={{ display: "flex", padding: "10px", gap: "3px", border: "1px solid black", width: "318px", flexWrap: "wrap" }}>
+                    {items.map((item) => (
+                        <OneBlock key={item.id} hasItem={item.hasItem} clicked={item.clicked} divClick={() => clickedItems(item.id)} />
+                    ))}</div>}
             <div>
-                <strong>Попытка: </strong>
+                <strong>Попытка: {resultTries.length}</strong>
             </div>
             <button onClick={reset}>Сброс</button>
         </>
